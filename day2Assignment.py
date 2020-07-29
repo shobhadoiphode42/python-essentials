505aa5626351d75f5e5d62aa3d202d5c61e0a4f2
shobhadoiphode42 <61405748+shobhadoiphode42@users.noreply.github.com>
2020-07-29 13:39:14 +0530

    Add files via upload

---
+++ day2Assignment.py
@@ -0,0 +1,429 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+print("hello python")
+
+
+# In[3]:
+
+
+print(""" ═ ╔ ╝ ╚ ╗ ║ ╣ ╬ ╠ ─ ╩ ╦ """)
+
+
+# In[4]:
+
+
+print("Hello Word")
+
+
+# In[5]:
+
+
+print('hello word')
+
+
+# In[6]:
+
+
+print("Python's Word")
+
+
+# In[7]:
+
+
+print("hello\tpython")
+
+
+# In[8]:
+
+
+print("hello\npython")
+
+
+# In[9]:
+
+
+print('Python\'s Word')
+
+
+# In[10]:
+
+
+print("hello\a")
+
+
+# In[13]:
+
+
+name="xyz"
+marks=90.12
+age=20
+print("The name of personis",name,"marks is ",marks,"age is",age)
+
+
+# In[14]:
+
+
+print("the name of person is %s marks is %f age is %d"%(name,marks,age))
+
+
+# In[15]:
+
+
+print(f"the name is{name} marks is{marks} age is{age}")
+
+
+# In[17]:
+
+
+cisco_temp=40
+
+
+# In[18]:
+
+
+x=y=10
+
+
+# In[19]:
+
+
+x=20
+
+
+# In[20]:
+
+
+id(x)
+
+
+# In[21]:
+
+
+y=20
+id(y)
+
+
+# In[22]:
+
+
+del x
+
+
+# In[23]:
+
+
+x
+
+
+# In[24]:
+
+
+y
+
+
+# In[25]:
+
+
+del y
+
+
+# In[26]:
+
+
+y
+
+
+# In[27]:
+
+
+5**5
+
+
+# In[28]:
+
+
+10+10
+
+
+# In[29]:
+
+
+20-10
+
+
+# In[30]:
+
+
+10/2
+
+
+# In[31]:
+
+
+10/3
+
+
+# In[32]:
+
+
+10%3
+
+
+# In[33]:
+
+
+10//3
+
+
+# In[34]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[35]:
+
+
+c=10
+a==c
+
+
+# In[36]:
+
+
+c<a
+
+
+# In[37]:
+
+
+c<=a
+
+
+# In[38]:
+
+
+a>b
+
+
+# In[39]:
+
+
+a>=b
+
+
+# In[40]:
+
+
+a!=b
+
+
+# In[41]:
+
+
+bin(b)
+
+
+# In[42]:
+
+
+a&b
+
+
+# In[43]:
+
+
+True or True
+
+
+# In[44]:
+
+
+True or False
+
+
+# In[45]:
+
+
+False or False
+
+
+# In[46]:
+
+
+False or True
+
+
+# In[47]:
+
+
+True and True
+
+
+# In[48]:
+
+
+True and False
+
+
+# In[49]:
+
+
+False and False
+
+
+# In[50]:
+
+
+False and True
+
+
+# In[51]:
+
+
+a=1
+b=20
+a>20
+a>20 or 10>9
+
+
+# In[52]:
+
+
+a>20 and 10>9
+
+
+# In[53]:
+
+
+10>9 and 20>m
+
+
+# In[54]:
+
+
+10>9 or 20>m
+
+
+# In[55]:
+
+
+10<9 and 20>m
+
+
+# In[56]:
+
+
+not True 
+
+
+# In[57]:
+
+
+str1="pythonprogramming"
+"a" in str1
+
+
+# In[58]:
+
+
+str1="pythonprogramming"
+"x" in str1
+
+
+# In[60]:
+
+
+str1="pythonprogramming"
+"x" not in str1
+
+
+# In[61]:
+
+
+a=10
+b=10
+a==b
+
+
+# In[62]:
+
+
+a is b
+
+
+# In[63]:
+
+
+id(a)
+
+
+# In[64]:
+
+
+id(b)
+
+
+# In[65]:
+
+
+x=1.5
+y=1.5
+x==y
+
+
+# In[66]:
+
+
+x is y
+
+
+# In[67]:
+
+
+id(x)
+
+
+# In[68]:
+
+
+id(y)
+
+
+# In[69]:
+
+
+a=257
+b=257
+a==b
+
+
+# In[70]:
+
+
+a is b
+
+
+# In[71]:
+
+
+10>8 and 8>9
+
+
+# In[ ]:
+
+
+
+
